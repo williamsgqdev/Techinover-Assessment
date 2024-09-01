be39e8cc29d51965c35b95fdb38bcaaf5a3b801d
williamsgqdev <williamsagbunu@gmail.com>
2024-09-01 17:23:24 +0100

    wip: signup feature

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  /**
+   * Signup
+   */
+  signup() {}
+  /**
+   * Login
+   */
+  login() {}
+}
